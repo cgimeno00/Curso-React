e266646fd978954887d244822e504d0eb9486b45
carlos <carlosgimenoortiz@gmail.com>
2024-01-31 19:01:42 +0100

    Tipado variables

--- Proyectos/0-8-traductor/src/types.d.ts
+++ Proyectos/0-8-traductor/src/types.d.ts
@@ -1,14 +1,19 @@
-export interface State{
-    fromLanguage:string
-    toLanguage:string
-    fromText:string
-    result:string
-    loading:boolean
+import { type AUTO_LANGUAGE, type SUPPORTED_LANGUAGES } from './constants'
+
+export interface State {
+  fromLanguage: string
+  toLanguage: string
+  fromText: string
+  result: string
+  loading: boolean
 }
 
+export type Language = keyof typeof SUPPORTED_LANGUAGES
+export type AutoLanguage = typeof AUTO_LANGUAGE
+export type FromLanguage = Language | AutoLanguague
 export type Action =
-|{type : 'SET_FROM_LANGUAGE' , payload:string}
-|{type : 'INTERCHANGE_LANGUAGES'}
-|{type : 'SET_TO_LANGUAGE', payload: string}
-|{type:'SET_FROM_TEXT', payload:string}
-|{type:'SET_RESULT', payload:string}
+| { type: 'SET_FROM_LANGUAGE', payload: string }
+| { type: 'INTERCHANGE_LANGUAGES' }
+| { type: 'SET_TO_LANGUAGE', payload: string }
+| { type: 'SET_FROM_TEXT', payload: string }
+| { type: 'SET_RESULT', payload: string }
