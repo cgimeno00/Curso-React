2d4f6c7a70305e550009f85b20952813cadfef83
carlos <carlosgimenoortiz@gmail.com>
2024-01-31 19:04:27 +0100

    Añadidos componentes bootstrap

--- Proyectos/0-8-traductor/src/App.tsx
+++ Proyectos/0-8-traductor/src/App.tsx
@@ -1,82 +1,30 @@
-
+/* eslint-disable react/react-in-jsx-scope */
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { useReducer } from 'react'
-import {Action, type  State } from './types.d'
+import { Container, Row, Col } from 'react-bootstrap'
+import { useStore } from './hooks/useStore'
 
+function App (): JSX.Element {
+  const { fromLanguage, setFromLanguage } = useStore()
 
-
-
-
-// Como hacer un reducer y useReducer
-// 1 --> Crear Estado inicial
-
-const initialState : State = {
-
-  fromLanguage:'auto',
-  toLanguage:'en',
-  fromText:'',
-  result:'',
-  loading:false
-}
-
-// 2 --> Creare una funcion reducer  Recibe un estado y una accion
-
-function reducer(state:State,action:Action){
-
-  // Recuperamos la accion y el payload
-  const {type} = action
-  
-  if(type==='INTERCHANGE_LANGUAGES'){
-    return{
-      ...state,
-      fromLanguage:state.toLanguage,
-      toLanguage:state.fromLanguage
-    }
-  }
-
-  if(type==='SET_FROM_LANGUAGE'){
-    return{
-      ...state,
-      fromLanguage:action.payload
-    }
-  }
-
-  if(type==='SET_TO_LANGUAGE'){
-    return{
-      ...state,
-      ToLanguage:action.payload
-    }
-  }
-
-  if(type==='SET_FROM_TEXT'){
-    return{
-      ...state,
-      loading:true,
-      fromText:action.payload,
-      result:''
-    }
-  }
-  if(type==='SET_RESULT'){
-    return{
-      ...state,
-      loading:false,
-      result:action.payload
-    }
-  }
-  return state
-}
-
-
-function App () {
-
-  // 3 --> Usamos hook useReducer para recibir el estado 
-
-  const[state, dispatch]=useReducer(reducer,initialState)
   return (
-    <>
-     <h1>Google Translate</h1>
-    </>
+   <Container fluid>
+    <h1>Google Translate</h1>
+    <Row>
+      <Col>
+      <h2>From</h2>
+      </Col>
+      <Col>
+      <h2>Change</h2>
+      </Col>
+      <Col>
+      <h2>To</h2>
+      </Col>
+    </Row>
+    <button onClick={() => { setFromLanguage('es') }}>Cambiar a español</button>
+    {fromLanguage}
+   </Container>
   )
 }
 
